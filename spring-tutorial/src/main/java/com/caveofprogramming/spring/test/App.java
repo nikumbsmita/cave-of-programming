408e63422d6cab0e26274d2ef206b9442464180f
nikumbsmita <nikumbsmita@gmail.com>
2017-04-02 12:53:55 -0700

    Constructor Arguments

--- spring-tutorial/src/main/java/com/caveofprogramming/spring/test/App.java
+++ spring-tutorial/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -1,17 +1,24 @@
 package com.caveofprogramming.spring.test;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+//class Path Context
 
 public class App {
 
 	public static void main(String[] args) {
 		
 		
-		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
 		
 		Person person = (Person)context.getBean("person");
 		person.speak();
+		
+		System.out.println(person);
+		
+		((ClassPathXmlApplicationContext)context).close();
 	}
 
 }
--- spring-tutorial/src/main/java/com/caveofprogramming/spring/test/Person.java
+++ spring-tutorial/src/main/java/com/caveofprogramming/spring/test/Person.java
@@ -2,8 +2,28 @@ package com.caveofprogramming.spring.test;
 
 public class Person {
 	
+	private int id;
+	private String name;
+	
+	public Person(){
+		
+	}
+	
+	public Person(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
 	public void speak(){
 		
 		System.out.println("Hello, I'm a person.");
 	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + "]";
+	}
+	
+	
 }
