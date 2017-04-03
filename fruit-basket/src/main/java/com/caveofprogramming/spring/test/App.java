b712679f3cee4f5d4c0686b98576f6255a51d08e
nikumbsmita <nikumbsmita@gmail.com>
2017-04-03 13:48:21 -0700

    Lists of Beans

--- fruit-basket/src/main/java/com/caveofprogramming/spring/test/App.java
+++ fruit-basket/src/main/java/com/caveofprogramming/spring/test/App.java
@@ -8,9 +8,9 @@ public class App {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("com/caveofprogramming/spring/test/beans/beans.xml");
 		
-		FruitBasket basket = (FruitBasket)context.getBean("basket");
+		Jungle jungle = (Jungle)context.getBean("jungle");
 		
-		System.out.println(basket);
+		System.out.println(jungle);
 		
 		((ClassPathXmlApplicationContext)context).close();
 
