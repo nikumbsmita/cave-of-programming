5e6d22cb7b63ea8c00e51027d025e91e102679c7
nikumbsmita <nikumbsmita@gmail.com>
2017-04-02 21:28:35 -0700

    The P NameSpace

--- spring-tutorial/src/main/java/com/caveofprogramming/spring/test/Address.java
+++ spring-tutorial/src/main/java/com/caveofprogramming/spring/test/Address.java
@@ -4,6 +4,10 @@ public class Address {
 	
 	private String street;
 	private String postcode;
+	
+	public Address(){
+		
+	}
 	public Address(String street, String postcode) {
 		this.street = street;
 		this.postcode = postcode;
@@ -15,6 +19,18 @@ public class Address {
 	@Override
 	public String toString() {
 		return "Address [street=" + street + ", postcode=" + postcode + "]";
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(String postcode) {
+		this.postcode = postcode;
 	}
 	
 	
